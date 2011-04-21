ed41ddcdc5f7f2c10fc8ed731a1bb309da67dc57
Piotr Balut <balut.piotr@gmail.com>
2011-04-21 08:11:14 +0200

    Moved content of slaves into mss-bus-master.
    There would be no Slave object, there would be no Slave object...

--- include/mss-bus.h
+++ include/mss-bus.h
@@ -8,7 +8,7 @@
  * Launches a mss-master which takes control over the network, allowing connected slaves to exchange
  * data. Slaves are staticly assigned to the master. Note that mss_run_master is blocking function
  * and will continue to run master until mss_stop_master is called.
- * @param slaves A pointer to array of slaves managed by the master.
+ * @param slaves A pointer to array of slaves managed by the master. Each entry contains address of one slave in the network.
  * @param slave_count Amount of slaves in slaves. :P
  */
 void mss_run_master( const mss_addr* slaves, int slaves_count );
--- src/mss-bus-master.c
+++ src/mss-bus-master.c
@@ -3,18 +3,28 @@
 
 int keep_master_running;
 
-void mss_run_master( const mss_addr* slaves_array, int slaves_count ) {
-	// set up working environment
-	LoadSlaves();
+void mss_run_master( const mss_addr* slaves, int slaves_count ) {
+	/* set up working environment */
+
+	// An array containing packet counters for each slave. Array size is MAX_ADDR+1 - slave address is an index
+	// of it's counter.
+	int* packet_count = malloc( (MAX_ADDR + 1) * sizeof( int ) );
+	
+	// init mem
+	int i;
+	for( i = 0; i < MAX_ADDR; ++i )
+		packet_count[i] = 0;
+	}
+	
 	keep_master_running = 1;
 	
-	// main loop
+	/* main loop */
 	while( keep_master_running ) {
 		//TODO: ReceivePacket( timeout )
 		//TODO: realizacja maszyny stanow dot. zarzadzania transmisja
 	}
 	
-	// clean up
+	/* clean up */
 	FreeSlaves( slaves_array, slaves_count );
 }
 
