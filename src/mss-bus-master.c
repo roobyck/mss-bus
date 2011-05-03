6997d7eca50c836e56bd4268021cd8fc1aed9d72
Piotr Balut <balut.piotr@gmail.com>
2011-05-03 19:02:34 +0200

    mss-run-master() logic implemented.

--- src/mss-bus-master.c
+++ src/mss-bus-master.c
@@ -2,34 +2,57 @@
 #include "config.h"
 #include "mss-bus.h"
 #include "packet.h"
+#include "crc.h"
 
 int keep_master_running;
 
 void mss_run_master (const mss_addr* slaves, int slaves_count) {
     /* set up working environment */
-
-    /* An array containing packet counters for each slave. Array size is
-     * MAX_ADDR+1 - slave address is an index of it's counter.
-    */ 
-    int* packet_count = malloc( (MAX_ADDR + 1) * sizeof(int) );
-
-    /* init mem */
-    int i;
-    for( i = 0; i < MAX_ADDR; ++i )
-        packet_count[i] = 0;
-    }
-
     keep_master_running = 1;
 
     MssPacket* packet = (MssPacket*) malloc( sizeof(MssPacket) );
+    /* Array of BUSes to pre-calculate crc checksums. */
+    MssPacket* bus_packet = (MssPacket*) malloc( slaves_count * sizeof(MssPacket) );
+    int i;
+    for( i = 0; i < MAX_ADDR; ++i ) {
+        BusPacket* p = (BusPacket)(bus_packet + i);
+        p->slave_addr = slaves[ i ];
+        p->packet_type = MSS_BUS;
+        p->crc = crc( p+2, 2 );
+            /* sizeof(mss_packet_type)+sizeof(mss_addr) zamiast 2? */
+            /* TODO: Makrodefinicje CRC_FOR_BUS, CRC_FOR_DAT, etc. */        
+    }
+    int current_slave = 0;
     
     /* main loop */
     while( keep_master_running ) {
-
-    }
+        
+        /* Send BUS to current slave. */
+        send_mss_packet( bus_packet + current_slave );
+    
+        /* Wait for NRQ, timeout, or data message. */
+        int recv_res = receive_mss_packet( packet, MSS_TIMEOUT );
+        if(
+            (recv_res != MSS_OK) || (
+                (recv_res == MSS_OK) &&
+                (( (GenericPacket) packet )->packet_type == MSS_DAT)
+            )    
+        ) {
+            /* Unless DAT is broadcast, we expect an ACK packet to appear. */
+            if( ((DataPacket) packet)->dst_addr != BROADCAST_ADDR )
+                int recv_res = receive_mss_packet( packet, MSS_TIMEOUT );
+        } /* else current slave has nothing to transfer. */
+        
+        /* Shift to next slave. */
+        ++current_slave;
+        if( current_slave == slaves_count )
+            current_slave = 0;
+        
+    } /* while */
 
     /* clean up */
-    free( packet_count );
+    free( packet );
+    free( bus_packet );
 }
 
 void mss_stop_master (void)
